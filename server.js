275d4581554d20951fcdf30f67b7bf067f5c4fed
Hansung Lee <hanlee@gold-point.com>
2019-04-04 15:32:13 -0700

    changed listen to static IP

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+var staticIP = "192.168.10.40";
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -146,7 +147,7 @@ app.delete("/todos/:id", function (req, res) {
 });
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
+app.listen(PORT, staticIP, function () {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: " + staticIP + ":" + PORT);
 });
