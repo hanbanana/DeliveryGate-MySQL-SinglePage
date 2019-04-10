50012c5df04ae8e1f5aadbc73b50ef54a3bccd2d
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-04-10 13:40:58 -0700

    render /

--- server.js
+++ server.js
@@ -42,19 +42,29 @@ app.get("/", function (req, res) {
       return res.status(500).end();
     }
 
-    res.render("wrongPage", { deliveryOrder: data });
-  });
-});
-
-app.get("/gate/DeliveryOrder", function (req, res) {
-  connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
     res.render("index", { deliveryOrder: data });
   });
 });
+
+// app.get("/", function (req, res) {
+//   connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
+//     if (err) {
+//       return res.status(500).end();
+//     }
+
+//     res.render("wrongPage", { deliveryOrder: data });
+//   });
+// });
+
+// app.get("/gate/DeliveryOrder", function (req, res) {
+//   connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
+//     if (err) {
+//       return res.status(500).end();
+//     }
+
+//     res.render("index", { deliveryOrder: data });
+//   });
+// });
 
 app.get("/create", function (req, res) {
   connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
